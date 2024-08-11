e10f5ba1ee071a5d7643431da655fa096b7be49b
Aniket Boryal <148935756+Aniket1103200@users.noreply.github.com>
2024-08-11 21:34:19 +0530

    initial commit after create custom exception

---
+++ backend/src/main/java/com/quiz/exception/customException/ResourseNotFoundException.java
@@ -0,0 +1,9 @@
+package com.quiz.exception.customException;
+
+public class ResourseNotFoundException extends RuntimeException {
+
+	public ResourseNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ backend/src/main/java/com/quiz/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.quiz.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+}
