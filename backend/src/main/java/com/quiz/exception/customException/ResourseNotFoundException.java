3e4ef9253644eed9f828458b50db30f9114411b4
Aniket Boryal <148935756+Aniket1103200@users.noreply.github.com>
2024-08-12 10:37:39 +0530

    successfully implement global exception handler.

--- backend/src/main/java/com/quiz/exception/customException/ResourseNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.quiz.exception.customException;
-
-public class ResourseNotFoundException extends RuntimeException {
-
-	public ResourseNotFoundException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-}
